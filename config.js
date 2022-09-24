96d2718203e015f6a5f389df802a28ac2f494709
Joniexy <113992122+Joniexy@users.noreply.github.com>
2022-09-24 08:46:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['628811520470']
+global.ownernomer = "628811520470"
+global.premium = ['628811520470']
+global.packname = 'Sticker  BangJul BOT'
+global.author = 'JULIAN SAPUTRA '
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
